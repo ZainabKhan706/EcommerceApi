afaaf7cd3939e3d033b316e0282c38a5d6c03918
Zainab Khan <zainabkhan.zk434@gmail.com>
2021-02-04 11:20:26 +0500

    Product Collection

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
-     * Transform the resource collection into an array.
+     * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [ 
+            'name'=>$this->name,
+            'discount'=>$this->discount,
+            'totalPrice'=>round(( 1 - ($this->discount/100))*$this->price,2),
+            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2):'No Rating Yet',
+            'href'=>[
+                'review'=>route('reviews.index',$this->id)
+            ]
+        ];
     }
 }
